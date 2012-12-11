6587a93196bbca5f6d4f17d7568386e6b2098864
Seven <seven1@51shepherd.com>
2012-12-11 12:32:58 +0800

    Implement #1 LED

---
+++ seven1/led.rb
@@ -0,0 +1,29 @@
+CHARS= {
+  a: "    ",
+  b: " -- ",
+  c: "|   ",
+  d: "   |",
+  e: "|  |",
+}
+
+NUMBERS= {
+   "0" => [:b, :e, :a, :e, :b],
+   "1" => [:a, :d, :a, :d, :a],
+   "2" => [:b, :d, :b, :c, :b],
+   "3" => [:b, :d, :b, :d, :b],
+   "4" => [:a, :e, :b, :d, :a],
+   "5" => [:b, :c, :b, :d, :b],
+   "6" => [:b, :c, :b, :e, :b],
+   "7" => [:b, :d, :a, :d, :a],
+   "8" => [:b, :e, :b, :e, :b],
+   "9" => [:b, :e, :b, :d, :b],
+}   
+def display(numbers)
+    numbers.each_char do |number|
+            NUMBERS[number].each do |x|
+                puts CHARS[x]
+            end
+    end
+end
+
+display(ARGV[0])
