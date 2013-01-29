d89d1c409acf8c0fafb62a7b23b164b965aef10b
Michael <kickyourass09@gmail.com>
2013-01-29 12:00:36 +0800

    make code easier to read, and write a rspec about it

--- michael/nokogiri/transfer2.rb
+++ michael/nokogiri/transfer2.rb
@@ -8,33 +8,32 @@ class Transfer
     @xml = Nokogiri::XML "<xml></xml>"
   end
 
-  def transfer
+  def transfer_to_xml
     @doc.css("span.attention_bold").each do |title| 
       rule = @xml.create_element "rule"
       xml_title = @xml.create_element "title"
       xml_title.content = title.content
       rule.add_child(xml_title)
-      xml_links = @xml.create_element "links"
-        
-      while title.next !=nil and title.next.name != "span" 
-        if title.next.name == "a"
-          link =title.next
-          xml_link =  @xml.create_element "link"
-          xml_link['url']=link.attributes["href"].value
-          xml_link.content= link.content
+      xml_links = @xml.create_element "links"      
+      next_node = title.next
+      while next_node && next_node.name != "span" 
+        if next_node.name == "a"
+          xml_link = @xml.create_element "link"
+          xml_link['url'] = next_node.attributes["href"].value
+          xml_link.content= next_node.content
           xml_links.add_child(xml_link)
         end
-        title = title.next        
+        next_node = next_node.next
       end
       rule.add_child(xml_links)
       @xml.child.add_child(rule)
     end
-    @xml.to_xml
+    @xml
   end
 
   def store_in_file
     ouput_file = File.new("ouput.xml","w")
-    ouput_file.puts transfer()
+    ouput_file.puts transfer_to_xml.to_xml
     ouput_file.close
   end
 end
---
+++ michael/nokogiri/transfer_spec.rb
@@ -0,0 +1,48 @@
+require_relative "transfer2"
+require "awesome_print"
+describe Transfer do 
+  let!(:url) { "index.html" }
+  let!(:transfer) { Transfer.new(url)}
+
+  describe "#transfer" do
+    it "shoud have right child tree" do
+      document = transfer.transfer_to_xml
+      xml = document.child
+      rule = xml.child
+      title = rule.children.first
+      links = rule.children[1]
+      link = links.child
+
+
+      document.name.should == "document"
+      xml.name.should == "xml"
+      rule.name.should == "rule"
+      title.name.should == "title"
+      links.name.should == "links"
+      link.name.should == "link"
+    end
+
+    it "should return the first rule" do
+      document = transfer.transfer_to_xml
+      xml = document.child
+      rule = xml.child
+      title = rule.children.first
+      links = rule.children[1]
+      link = links.child
+
+      title.content.should eq "AGING, Texas Department on"
+      link.attributes["url"].value.should eq "/texreg/transfers/aging091004.html"
+    end
+
+    it "should return the numbers of rule " do
+      document = transfer.transfer_to_xml
+      xml = document.child
+      rules = xml.children
+      rules.count.should == 44
+    end
+
+  end
+
+
+
+end
